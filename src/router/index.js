3370c185759df416f93dd895e963b351d39b5166
B2014776 <leminhphuong899@gmail.com>
2024-04-03 14:14:41 +0700

    them trang lien he yeu thich

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,11 @@ const routes = [
     component: ContactBook,
   },
   {
+    path: "/favorite",
+    name: "contactbook.favorite",
+    component: () => import("@/views/ContactFavorite.vue"),
+  },
+  {
     path: "/:pathMatch(.*)*",
     name: "notfound",
     component: () => import("@/views/NotFound.vue"),
@@ -16,6 +21,12 @@ const routes = [
     path: "/contacts/:id",
     name: "contact.edit",
     component: () => import("@/views/ContactEdit.vue"),
+    props: true, // Truyền các biến trong $route.params vào làm props
+  },
+  {
+    path: "/favorite",
+    name: "contact.favorite",
+    component: () => import("@/views/ContactFavorite.vue"),
     props: true, // Truyền các biến trong $route.params vào làm props
   },
   {
